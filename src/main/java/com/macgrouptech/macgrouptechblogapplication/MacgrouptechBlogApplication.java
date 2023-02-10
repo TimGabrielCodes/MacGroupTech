7e38da936eb786c96f08079288d5a1a0ea1712ac
Zinwota Timothy <zinwotatimothy@gmail.com>
2023-02-10 14:02:58 +0100

    Init

---
+++ src/main/java/com/macgrouptech/macgrouptechblogapplication/MacgrouptechBlogApplication.java
@@ -0,0 +1,13 @@
+package com.macgrouptech.macgrouptechblogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacgrouptechBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacgrouptechBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macgrouptech/macgrouptechblogapplication/MacgrouptechBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macgrouptech.macgrouptechblogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacgrouptechBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
